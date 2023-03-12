893ee33482720fc0af06a142b877c9a825a8e47d
yulong.l <62822897+yulong-liu-host@users.noreply.github.com>
2023-03-12 11:49:17 +0000

    Add files via upload

---
+++ SR04/hcsr04.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <wiringPi.h>   
+//#include <pigpio.h>
+#include <iomanip>
+#include "TimeUtils.h"
+
+if (gpioInitialise() < 0)
+{
+   // pigpio initialisation failed.
+}
+else
+{
+   // pigpio initialised okay.
+}
+
+//定义WiringPi引脚编号
+// For 1st Ultrasonic distance sensor
+const int trig = 20;
+const int echo = 21;
+// For 2nd sensor
+const int trig2 = 19;
+const int echo2 = 26;
+
+
+using namespace std;
+
+class hcsr04
+{
+public:
+	hcsr04();
+	~hcsr04();
+public:
+	//初始化引脚模式等
+	void static init();
+	//循环执行部分
+	void static loop();
+	//具体实现写在这里
+	void static implement();
+	void static implement2();
+};
+	
